9a49019c84a21bfb1dbd97f8dcc67cbf0bcd15de
alex <504770@student.fontys.nl>
2024-04-09 08:47:40 +0200

    test classes created but with failed tests

--- iam-enterprise-architecture-backend/src/bookings/bookings.controller.spec.ts
+++ iam-enterprise-architecture-backend/src/bookings/bookings.controller.spec.ts
@@ -1,1 +1,25 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { BookingsController } from './bookings.controller';
+import { BookingService } from './bookings.service';
+
+describe('BookingsController',()=>{
+    let controller:BookingsController;
+    let service:BookingService;
+
+
+    beforeEach(async()=>{
+        const module:TestingModule=await Test.createTestingModule({
+            controllers:[BookingsController],
+            providers:[BookingService]
+        }).compile();
+
+        controller=module.get<BookingsController>(BookingsController);
+        service=module.get<BookingService>(BookingService);
+    });
+
+    it('should be defined',()=>{
+        expect(controller).toBeDefined();
+    });
+});
+
+
