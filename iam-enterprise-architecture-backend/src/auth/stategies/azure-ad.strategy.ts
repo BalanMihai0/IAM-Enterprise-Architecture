54481c4fdb5035bb2972e34c9a1f1a2ce0f95d87
Augustinas Digrys <digrys.augustinas@gmail.com>
2024-05-13 23:14:15 +0200

    Updated env file var names to be reused with fe

--- iam-enterprise-architecture-backend/src/auth/stategies/azure-ad.strategy.ts
+++ iam-enterprise-architecture-backend/src/auth/stategies/azure-ad.strategy.ts
@@ -9,15 +9,15 @@ export class AzureADStrategy extends PassportStrategy(Strategy, 'azure-ad') {
   constructor(protected readonly configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      aud: configService.get('AZURE_AD_AUDIENCE'),
-      iss: `https://sts.windows.net/${configService.get('AZURE_AD_TENANTID')}/`,
+      aud: configService.get('MSAL_API_CLIENT_ID'),
+      iss: `https://sts.windows.net/${configService.get('MSAL_WEB_TENANT_ID')}/`,
       algorithms: ['RS256'],
       ignoreExpiration: false,
       secretOrKeyProvider: jwksRsa.passportJwtSecret({
         cache: true,
         rateLimit: true,
         jwksRequestsPerMinute: 5,
-        jwksUri: `https://login.microsoftonline.com/${configService.get('AZURE_AD_TENANTID')}/discovery/v2.0/keys`,
+        jwksUri: `https://login.microsoftonline.com/${configService.get('MSAL_WEB_TENANT_ID')}/discovery/v2.0/keys`,
       }),
     });
   }
