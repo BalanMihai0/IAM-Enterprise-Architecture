e65aba8edf9442784f0e0d4333b97e0df12382cf
KeyShr3kt <ionutjocuri2003@gmail.com>
2024-05-15 15:52:54 +0200

    Fix role for MSAL Account.

--- iam-enterprise-architecture-backend/src/auth/auth.controller.ts
+++ iam-enterprise-architecture-backend/src/auth/auth.controller.ts
@@ -1,6 +1,13 @@
-import { Body, Controller, Get, Post, Req, Res, UnauthorizedException, UseGuards } from '@nestjs/common';
-import * as cookieParser from 'cookie-parser';
-import { Request, Response } from 'express';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  UnauthorizedException,
+  UseGuards,
+} from '@nestjs/common';
+import { Request } from 'express';
 import { LocalGuard } from './guards/local.guard';
 import { JwtAuthGuard } from './guards/jwt.guard';
 import { Roles } from './roles/roles.decorator';
@@ -8,79 +15,90 @@ import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { AuthPayloadDto } from './dto/auth.dto';
 import { AzureADGuard } from './guards/azure-ad.guard';
 import { JwtService } from '@nestjs/jwt';
-import { AuthService } from './auth.service';
 import { jwtDecode } from 'jwt-decode';
 import * as jwt from 'jsonwebtoken';
 
 @ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
-    constructor(private jwtService: JwtService) { }
+  constructor(private jwtService: JwtService) {}
 
-    //endpoint to retrieve a refresh token if credentials were valid (LocalGuard does the Auth)
-    @Post('login')
-    @Roles('*')
-    @UseGuards(LocalGuard)
-    login(@Req() req: Request, @Body() authDto: AuthPayloadDto) {
-        return req.user;
+  //endpoint to retrieve a refresh token if credentials were valid (LocalGuard does the Auth)
+  @Post('login')
+  @Roles('*')
+  @UseGuards(LocalGuard)
+  login(@Req() req: Request, @Body() authDto: AuthPayloadDto) {
+    return req.user;
+  }
+
+  //endpoint to retrieve an actual access token if refresh token was valid
+  @Get('refresh')
+  @Roles('*')
+  loginRefresh(@Req() req: Request) {
+    const refreshToken = req.cookies.refreshToken;
+
+    try {
+      jwt.verify(refreshToken, process.env.JWT_SECRET_KEY);
+    } catch (error) {
+      throw new UnauthorizedException();
     }
 
-    //endpoint to retrieve an actual access token if refresh token was valid
-    @Get('refresh')
-    @Roles('*')
-    loginRefresh(@Req() req: Request) {
-        const refreshToken = req.cookies.refreshToken;
+    const decodedToken = jwtDecode(refreshToken);
 
-        try { 
-            jwt.verify(refreshToken, process.env.JWT_SECRET_KEY);
-        } catch (error) {
-            throw new UnauthorizedException;
-        }
+    const uniqueName = (decodedToken as any).unique_name;
 
-        const decodedToken = jwtDecode(refreshToken);
-        
-        const uniqueName = (decodedToken as any).unique_name;
-
-        let role = 'customer';
-        if (decodedToken.iss === 'https://sts.windows.net/00968c3f-7e86-471d-b6e2-5811aaae8d59/') {
-            role = 'admin';
-        }
-
-        const newToken = this.jwtService.sign({
-            unique_name: uniqueName,
-            iat: Math.floor(Date.now() / 1000),
-            role: role
-        }, { expiresIn: '30m' });
-
-        return newToken;
+    let role = 'customer';
+    if (
+      decodedToken.iss ===
+      'https://sts.windows.net/00968c3f-7e86-471d-b6e2-5811aaae8d59/'
+    ) {
+      role = 'admin';
     }
 
-    //endpoint to check if token is valid (JwtAuthGuard protects the endpoint)
-    @Get('status')
-    @Roles("customer", "admin")
-    @ApiBearerAuth()
-    @UseGuards(JwtAuthGuard)
-    status(@Req() req: Request) {
-        return req.user;
+    const newToken = this.jwtService.sign(
+      {
+        unique_name: uniqueName,
+        iat: Math.floor(Date.now() / 1000),
+        role: role,
+      },
+      { expiresIn: '30m' },
+    );
+
+    return newToken;
+  }
+
+  //endpoint to check if token is valid (JwtAuthGuard protects the endpoint)
+  @Get('status')
+  @Roles('customer', 'admin')
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
+  status(@Req() req: Request) {
+    return req.user;
+  }
+
+  @Get('refresh/ms')
+  @Roles('*')
+  @ApiBearerAuth()
+  @UseGuards(AzureADGuard)
+  async validateToken(@Req() req) {
+    let role = 'customer';
+    console.log(req.user);
+    if (
+      req.user.iss ===
+      `https://sts.windows.net/${process.env.MSAL_WEB_TENANT_ID}/`
+    ) {
+      role = 'admin';
     }
 
-    @Get('refresh/ms')
-    @Roles("*")
-    @ApiBearerAuth()
-    @UseGuards(AzureADGuard)
-    async validateToken(@Req() req) {
+    const newToken = this.jwtService.sign(
+      {
+        unique_name: req.user.unique_name,
+        iat: Math.floor(Date.now() / 1000),
+        role: role,
+      },
+      { expiresIn: '30m' },
+    );
 
-        let role = 'customer';
-        if (req.user.iss === `https://sts.windows.net/${process.env.AZURE_AD_TENANTID}/`) {
-            role = 'admin';
-        }
-
-        const newToken = this.jwtService.sign({
-            unique_name: req.user.unique_name,
-            iat: Math.floor(Date.now() / 1000),
-            role: role
-        }, { expiresIn: '30m' });
-
-        return newToken;
-    }
+    return newToken;
+  }
 }
