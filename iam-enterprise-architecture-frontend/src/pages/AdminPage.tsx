506785ce22cfbd6f15bc6c6b0eb8cc32b5745ba6
alex <504770@student.fontys.nl>
2024-05-30 09:50:10 +0200

    finish the design for the admin page.

--- iam-enterprise-architecture-frontend/src/pages/AdminPage.tsx
+++ iam-enterprise-architecture-frontend/src/pages/AdminPage.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 // @ts-nocheck
 import { BrowserRouter as Routes,Route, Link } from 'react-router-dom';
+import { useState } from 'react';
 import {
   Card,
   Typography,
@@ -16,7 +17,7 @@ import '../style/landingPage.css';
 import UsersAdminPage from './UsersAdminPage';
 import JobsAdminPage from './JobsAdminPage.tsx';
 
-const Sidebar = () => (
+const Sidebar = ({ setSelectedPage }) => (
   <Card className="h-[calc(100vh-2rem)] w-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5">
     <div className="mb-2 p-4">
       <Typography variant="h5" color="blue-gray">
@@ -24,38 +25,34 @@ const Sidebar = () => (
       </Typography>
     </div>
     <List>
-      <Link to="/admin/jobs">
-        <ListItem>
-          <ListItemPrefix>
-            <PresentationChartBarIcon className="h-5 w-5" />
-          </ListItemPrefix>
-          Jobs
-        </ListItem>
-      </Link>
-      <Link to="/admin/users">
-        <ListItem>
-          <ListItemPrefix>
-            <UserCircleIcon className="h-5 w-5" />
-          </ListItemPrefix>
-          Users
-        </ListItem>
-      </Link>
+      <ListItem onClick={() => setSelectedPage('jobs')}>
+        <ListItemPrefix>
+          <PresentationChartBarIcon className="h-5 w-5" />
+        </ListItemPrefix>
+        Jobs
+      </ListItem>
+      <ListItem onClick={() => setSelectedPage('users')}>
+        <ListItemPrefix>
+          <UserCircleIcon className="h-5 w-5" />
+        </ListItemPrefix>
+        Users
+      </ListItem>
     </List>
   </Card>
 );
 
-const AdminPage = () => (
-  
+const AdminPage = () => {
+  const [selectedPage, setSelectedPage] = useState('jobs');
+
+  return (
     <div className="flex">
-      <Sidebar />
+      <Sidebar setSelectedPage={setSelectedPage} />
       <div className="w-3/4 p-5">
-        {/* <Routes>
-          <Route element={<JobsAdminPage />} />
-          <Route element={<UsersAdminPage />} />
-        </Routes> */}
+        {selectedPage === 'jobs' && <JobsAdminPage />}
+        {selectedPage === 'users' && <UsersAdminPage />}
       </div>
     </div>
-
-);
+  );
+};
 
 export default AdminPage;
