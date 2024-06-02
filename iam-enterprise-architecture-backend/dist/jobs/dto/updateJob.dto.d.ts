bc174240f25278035125e4f28cd6d066f0d1e818
Dimitar Atanasov <sencor0442@gmail.com>
2024-06-02 23:59:48 +0200

    removed dist from git cache

--- iam-enterprise-architecture-backend/dist/jobs/dto/updateJob.dto.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class UpdateJobDto {
-    description?: string;
-    price?: number;
-    type?: string;
-}
--- iam-enterprise-architecture-backend/dist/users/dto/user.dto.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class NewUserDto {
-    fullName: string;
-    email: string;
-    password: string;
-    confirmPassword: string;
-}
