445d1aa12adb54a7be09f399e39297b02ff3e509
KeyShr3kt <ionutjocuri2003@gmail.com>
2024-06-03 13:29:53 +0200

    Optimised bookings page.

--- iam-enterprise-architecture-frontend/src/pages/BookingsPage.tsx
+++ iam-enterprise-architecture-frontend/src/pages/BookingsPage.tsx
@@ -1,56 +1,75 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-// @ts-nocheck
-
-import { useEffect, useState } from 'react';
-import { Card, CardHeader, CardBody, Typography } from "@material-tailwind/react";
-import { getTokenUniqueName } from '../context/AuthContext';
-import { useAuth } from '../context/AuthContext';
-import BookingCard from '../components/booking/BookingCard';
-import Sidebar from '../components/booking/Sidebar';
-import { axiosInstance } from '../api/AxiosConfig';
+import { useEffect, useState } from "react";
+import { Typography } from "@material-tailwind/react";
+import BookingCard from "../components/booking/BookingCard";
+import Sidebar from "../components/booking/Sidebar";
+import { axiosInstance } from "../api/AxiosConfig";
+import { useAuth } from "../context/AuthContext";
+import { jwtDecode } from "jwt-decode";
 
 const BookingsPage = () => {
   const [allBookings, setAllBookings] = useState([]);
   const [bookings, setBookings] = useState([]);
-  const [userId, setUserId] = useState(getTokenUniqueName());
+  const [userId, setUserId] = useState<string | null>(null);
+  const [selectedFilter, setSelectedFilter] = useState<string>("all");
+  const [searchQuery, setSearchQuery] = useState<string>("");
   const { accessToken } = useAuth();
-  const [selectedFilter, setSelectedFilter] = useState<string>('all');
-  const [searchQuery, setSearchQuery] = useState<string>('');
-
-  const fetchBookings = async () => {
-    try {
-      if (!userId) return console.error('No user ID found');
-
-      const response = await axiosInstance.get(`/api/v1/bookings/user/${userId}`);
-      setAllBookings(response.data);
-      setBookings(response.data);
-    } catch (error) {
-      console.error('Error fetching bookings:', error);
-    }
-  };
 
   useEffect(() => {
+    const fetchBookings = async () => {
+      try {
+        //@ts-expect-error - unique_name is a property of the decoded token
+        setUserId(jwtDecode(accessToken as string)?.unique_name || null);
+        if (!userId) return console.error("No user ID found");
+
+        const response = await axiosInstance.get(
+          `/api/v1/bookings/user/${userId}`
+        );
+        setAllBookings(response.data);
+        setBookings(response.data);
+      } catch (error) {
+        console.error("Error fetching bookings:", error);
+      }
+    };
+
     fetchBookings();
-  }, []);
+  }, [accessToken, userId]);
 
   useEffect(() => {
-    if (selectedFilter === 'all') {
+    if (selectedFilter === "all") {
       setBookings(allBookings);
-    }
-    else if (selectedFilter === 'ongoing') {
-      setBookings(allBookings.filter((booking) => new Date(booking.startDate) <= new Date() && new Date(booking.endDate) >= new Date()));
-    }
-    else if (selectedFilter === 'past') {
-      setBookings(allBookings.filter((booking) => new Date(booking.endDate) < new Date()));
-    }
-    else if (selectedFilter === 'upcoming') {
-      setBookings(allBookings.filter((booking) => new Date(booking.startDate) > new Date()));
+    } else if (selectedFilter === "ongoing") {
+      setBookings(
+        allBookings.filter(
+          (booking) =>
+            //@ts-expect-error - startDate is a property of booking
+            new Date(booking.startDate) <= new Date() &&
+            //@ts-expect-error - endDate is a property of booking
+            new Date(booking.endDate) >= new Date()
+        )
+      );
+    } else if (selectedFilter === "past") {
+      setBookings(
+        //@ts-expect-error - endDate is a property of booking
+        allBookings.filter((booking) => new Date(booking.endDate) < new Date())
+      );
+    } else if (selectedFilter === "upcoming") {
+      setBookings(
+        allBookings.filter(
+          //@ts-expect-error - startDate is a property of booking
+          (booking) => new Date(booking.startDate) > new Date()
+        )
+      );
     }
 
     if (searchQuery) {
-      setBookings(bookings.filter((booking) => booking.job.title.toLowerCase().includes(searchQuery.toLowerCase())));
+      setBookings(
+        bookings.filter((booking) =>
+          //@ts-expect-error - job is a property of booking
+          booking.job.title.toLowerCase().includes(searchQuery.toLowerCase())
+        )
+      );
     }
-  }, [selectedFilter, searchQuery]);
+  }, [selectedFilter, searchQuery, allBookings, bookings]);
 
   const handleSelectedFilterChange = (filter: string) => {
     setSelectedFilter(filter);
@@ -69,12 +88,16 @@ const BookingsPage = () => {
           onSearch={handleSearch}
         />
       </div>
-      <div className='flex-1 overflow-y-auto p-4'>
-        <Typography variant="h2" className="text-center mb-8">
+      <div className="flex-1 overflow-y-auto p-4">
+        <Typography
+          variant="h2"
+          className="text-center mb-8"
+          placeholder={"typography"}
+        >
           Your Bookings
         </Typography>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-12">
-          {bookings.map((booking) => (
+          {bookings.map((booking: { id: string }) => (
             <BookingCard key={booking.id} booking={booking} />
           ))}
         </div>
